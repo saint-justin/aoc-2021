865b087ec5ce39c106687ddf9d929e04f3b22f47
Justin Vaughn <jtv6445@g.rit.edu>
2021-12-04 19:34:43 -0800

    Finished Day 3

--- Day3/part2.py
+++ Day3/part2.py
@@ -1,53 +1,53 @@
 import pathlib
-path = f'{pathlib.Path().resolve()}/aoc-2021/Day3'
+path = f'{pathlib.Path().resolve()}/Day3'
 
-def reduce_for_oxy(arr, bit_pos):
-  # print(f'\nBit Position: {bit_pos}')
+def reduce(arr, bit_pos, a, b):
+  if len(arr) == 1: 
+    return arr
+
   diff = 0
   for line in arr:
     if line[bit_pos] == '1': diff += 1
     else: diff -= 1
 
-  # print(f'Diff: {diff} ==> Preferring {"1" if diff <= 0 else "0"}')
-
   out = []
   for line in arr:
-    if diff >= 0 and line[bit_pos] == '1': out.append(line)
-    elif diff < 0 and line[bit_pos] == '0': out.append(line)
+    if diff >= 0 and line[bit_pos] == a: out.append(line)
+    elif diff < 0 and line[bit_pos] == b: out.append(line)
   return out
+
+def reduce_for_oxy(arr, bit_pos):
+  return reduce(arr, bit_pos, '1', '0')
+
+def reduce_for_co2(arr, bit_pos):
+  return reduce(arr, bit_pos, '0', '1')
 
 def dec_from_bin(arr):
   sum = 0
   current = 1
   for i in range(len(arr)-1, -1, -1):
-    sum += (current) * int(arr[i])
+    sum += current * int(arr[i])
     current *= 2
   return sum
 
-with open(f'{path}/input_test.txt') as f:
-  arr = []
+
+with open(f'{path}/input.txt') as f:
   std = []
-  inv = []
+  co2 = []
   for line in f:
     std.append(line[0:-1])
-    inv.append(line[0:-1])
-  
-  for line in f:
-    if len(arr) == 0:
-      for i in range(0, len(line)-1): 
-        arr.append(0)
-    else: break
+    co2.append(line[0:-1])
     
-  print('Starting cycles...')
-  print(std)
   position = 0
-  while len(std) > 1:
+  while True:
     std = reduce_for_oxy(std, position)
+    co2 = reduce_for_co2(co2, position)
+    
+    if len(std) == 1 and len(co2) == 1: 
+      break
+
     position += 1
-    # print(f'Standard: {std}')
-    # print(f'Inverted: {inv}')
 
   std_dec = dec_from_bin(std[0])
-  # inv_dec = dec_from_bin(inv[0])
-  # print(f'std: {std_dec}   inv: {inv_dec}   out: {std_dec * inv_dec}')
-  print(f'\nstd: {std_dec}   out: {std_dec } * inv_dec')
+  co2_dec = dec_from_bin(co2[0])
+  print(f'\nstd: {std_dec}   co2: {co2_dec}   out: {std_dec * co2_dec}')
